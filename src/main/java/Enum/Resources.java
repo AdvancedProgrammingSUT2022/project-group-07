13c22048a178b35d248fa4b9aa08abc7e67cf09b
Danial Gharib <danial.mail.gh@gmail.com>
2022-04-18 02:24:01 +0430

    Resources completed!!

--- src/main/java/Enum/Resources.java
+++ src/main/java/Enum/Resources.java
@@ -1,25 +1,25 @@
 package Enum;
 
 public enum Resources {
-    BANANA("banana", 1, 0, 0, new String[]{"forest"}, null, null, TypeOfResource.BONUS),
-    COW("cow", 1, 0, 0, new String[]{"grassland"}, null, null, TypeOfResource.BONUS),
-    GAZELLE("gazelle", 1, 0, 0, new String[]{"forest", "tundra", "hill"}, null, null, TypeOfResource.BONUS),
-    SHEEP("sheep", 2, 0, 0, new String[]{"hill", "grassland", "", "desert"}, null, null, TypeOfResource.BONUS),
-    WHEAT("wheat", 1, 0, 0, new String[]{"plain", "floodplains"}, null, null, TypeOfResource.BONUS),
-    COAL("coal", 0, 1, 0, new String[]{"plain", "hill", "grassland"}, null, null, TypeOfResource.STRATEGIC),
-    HORSE("horse", 0, 1, 0, new String[]{"tundra", "plain", "grassland"}, null, null, TypeOfResource.STRATEGIC),
-    IRON("iron", 0, 1, 0, new String[]{"tundra", "plain", "desert", "hill", "grassland", "snow"}, null, null, TypeOfResource.STRATEGIC),
-    COTTON("cotton", 0, 0, 2, new String[]{"desert", "plain", "grassland"}, null, null, TypeOfResource.LUXURY),
-    COLOR("color", 0, 0, 2, new String[]{"jungle", "forest"}, null, null, TypeOfResource.LUXURY),
-    FUR("fur", 0, 0, 2, new String[]{"forest", "tundra"}, null, null, TypeOfResource.LUXURY),
-    PRECIOUS_STONE("preciousStone", 0, 0, 3, new String[]{"jungle", "tundra", "plain", "desert", "grassland", "hill"}, null, null, TypeOfResource.LUXURY),
-    GOLD("gold", 0, 0, 2, new String[]{"desert", "plain", "hill", "grassland"}, null, null, TypeOfResource.LUXURY),
-    ALOE("aloe", 0, 0, 2, new String[]{"plain", "desert"}, null, null, TypeOfResource.LUXURY),
-    IVORY("ivory", 0, 0, 2, new String[]{"plain"}, null, null, TypeOfResource.LUXURY),
-    MARBLE("marble", 0, 0, 2, new String[]{"tundra", "plain", "desert", "grassland", "hill"}, null, null, TypeOfResource.LUXURY),
-    SILK("silk", 0, 0, 2, new String[]{"forest"}, null, null, TypeOfResource.LUXURY),
-    SILVER("silver", 0, 0, 2, new String[]{"tundra", "desert", "hill"}, null, null, TypeOfResource.LUXURY),
-    SUGAR("sugar", 0, 0, 2, new String[]{"floodplains", "marsh"}, null, null, TypeOfResource.LUXURY);
+    BANANA("banana", 1, 0, 0, new String[]{"forest"}, null, TypeOfImprovement.PLANTATION, TypeOfResource.BONUS),
+    COW("cow", 1, 0, 0, new String[]{"grassland"}, null, TypeOfImprovement.PASTURE, TypeOfResource.BONUS),
+    GAZELLE("gazelle", 1, 0, 0, new String[]{"forest", "tundra", "hill"}, null, TypeOfImprovement.CAMP, TypeOfResource.BONUS),
+    SHEEP("sheep", 2, 0, 0, new String[]{"hill", "grassland", "", "desert"}, null, TypeOfImprovement.PASTURE, TypeOfResource.BONUS),
+    WHEAT("wheat", 1, 0, 0, new String[]{"plain", "floodplains"}, null, TypeOfImprovement.FARM, TypeOfResource.BONUS),
+    COAL("coal", 0, 1, 0, new String[]{"plain", "hill", "grassland"}, TypeOfTechnology.SCIENTIFIC_THEORY, TypeOfImprovement.MINE, TypeOfResource.STRATEGIC),
+    HORSE("horse", 0, 1, 0, new String[]{"tundra", "plain", "grassland"}, TypeOfTechnology.ANIMAL_HUSBANDRY, TypeOfImprovement.PASTURE, TypeOfResource.STRATEGIC),
+    IRON("iron", 0, 1, 0, new String[]{"tundra", "plain", "desert", "hill", "grassland", "snow"}, TypeOfTechnology.IRON_WORKING, TypeOfImprovement.MINE, TypeOfResource.STRATEGIC),
+    COTTON("cotton", 0, 0, 2, new String[]{"desert", "plain", "grassland"}, null, TypeOfImprovement.PLANTATION, TypeOfResource.LUXURY),
+    COLOR("color", 0, 0, 2, new String[]{"jungle", "forest"}, null, TypeOfImprovement.PLANTATION, TypeOfResource.LUXURY),
+    FUR("fur", 0, 0, 2, new String[]{"forest", "tundra"}, null, TypeOfImprovement.CAMP, TypeOfResource.LUXURY),
+    PRECIOUS_STONE("preciousStone", 0, 0, 3, new String[]{"jungle", "tundra", "plain", "desert", "grassland", "hill"}, null, TypeOfImprovement.MINE, TypeOfResource.LUXURY),
+    GOLD("gold", 0, 0, 2, new String[]{"desert", "plain", "hill", "grassland"}, null, TypeOfImprovement.MINE, TypeOfResource.LUXURY),
+    ALOE("aloe", 0, 0, 2, new String[]{"plain", "desert"}, null, TypeOfImprovement.PLANTATION, TypeOfResource.LUXURY),
+    IVORY("ivory", 0, 0, 2, new String[]{"plain"}, null, TypeOfImprovement.CAMP, TypeOfResource.LUXURY),
+    MARBLE("marble", 0, 0, 2, new String[]{"tundra", "plain", "desert", "grassland", "hill"}, null, TypeOfImprovement.QUARRY, TypeOfResource.LUXURY),
+    SILK("silk", 0, 0, 2, new String[]{"forest"}, null, TypeOfImprovement.PLANTATION, TypeOfResource.LUXURY),
+    SILVER("silver", 0, 0, 2, new String[]{"tundra", "desert", "hill"}, null, TypeOfImprovement.MINE, TypeOfResource.LUXURY),
+    SUGAR("sugar", 0, 0, 2, new String[]{"floodplains", "marsh"}, null, TypeOfImprovement.PLANTATION, TypeOfResource.LUXURY);
 
 
     private String name;
