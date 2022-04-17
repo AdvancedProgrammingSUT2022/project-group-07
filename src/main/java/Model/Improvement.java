02c1937f1981d1608c09334f56b5fb850aef160e
Mana <mana@example.com>
2022-04-17 23:48:08 +0430

    improvement added

---
+++ src/main/java/Model/Improvement.java
@@ -0,0 +1,30 @@
+package Model;
+
+import Enum.TypeOfImprovement;
+import java.util.ArrayList;
+
+public class Improvement {
+    private TypeOfImprovement improvement;
+    private int turn;
+
+    public Improvement(TypeOfImprovement improvement, int turn) {
+        this.improvement = improvement;
+        this.turn = turn;
+    }
+
+    public TypeOfImprovement getImprovement() {
+        return improvement;
+    }
+
+    public void setImprovement(TypeOfImprovement improvement) {
+        this.improvement = improvement;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
+}
