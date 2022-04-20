09990c3277a2a85f3224b244986c9a171e244f19
Mana <mana@example.com>
2022-04-20 16:12:28 +0430

    Unit and Select controller added

---
+++ src/main/java/Controller/game/SelectController.java
@@ -0,0 +1,30 @@
+package Controller.game;
+
+import Model.City;
+import Model.Location;
+import Model.Unit;
+
+public class SelectController {
+    public static Unit selectedUnit;
+    public static City selectedCity;
+
+    private String selectNonCombatUnit(Location location) {
+        return "";
+    }
+
+    private String selectCombatUnit(Location location) {
+        return "";
+    }
+
+    private boolean positionIsValid(Location location) {
+        return true;
+    }
+
+    private String selectCity(String name) {
+        return "";
+    }
+
+    private String selectCity(Location location) {
+        return "";
+    }
+}
---
+++ src/main/java/Controller/game/UnitController.java
@@ -0,0 +1,39 @@
+package Controller.game;
+
+import Model.Location;
+import Model.Unit;
+
+public class UnitController {
+
+    public String moveUnit(Unit unit, Location location) {
+        return "";
+    }
+
+    public boolean hasOwnerShip(Unit unit) {
+        return true;
+    }
+
+    public String sleep(Unit unit) {
+        return "";
+    }
+
+    public void healUnit(Unit unit) {
+
+    }
+
+    public String cancelMission(Unit unit) {
+        return "";
+    }
+
+    public String wake(Unit unit) {
+        return "";
+    }
+
+    public String deleteUnit(Unit unit) {
+        return "";
+    }
+
+    public String upgrade(Unit unit) {
+        return "";
+    }
+}
