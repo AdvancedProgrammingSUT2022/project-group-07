8fc4512db2958d8cc2f051a76652ffea4e5fa7dd
Mahan-Bayhaghi <mahan.bayhaghi@gmail.com>
2022-04-20 23:56:32 +0430

    add civilizationInitialize

--- src/main/java/Controller/game/GameController.java
+++ src/main/java/Controller/game/GameController.java
@@ -11,9 +11,26 @@ public class GameController {
     private ArrayList<Civilization> civilizations;
     private int time;
     private int turn;
+    private Civilization currentCivilization ;
 
-    public GameController(ArrayList<User> users) {
-        //this.map = initilize();
+    private void initializeMap (){
+        // TODO: iterate on array and create our map
 
     }
+
+    private void initializeCivilizations (ArrayList<User> users){
+        // TODO: create list of civilizations
+        this.civilizations = new ArrayList<Civilization>();
+        for (int i = 0; i < users.size(); i++)
+            civilizations.add(new Civilization("c"+Integer.toString(i) , users.get(i))) ;
+    }
+
+    public GameController(ArrayList<User> users) {
+        initializeCivilizations(users);
+        //initializeMap();
+    }
+
+
+
+
 }
