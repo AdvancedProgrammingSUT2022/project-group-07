e4b838e5e0a31afc3a86a0527644607d86e6f2b7
Mahan-Bayhaghi <mahan.bayhaghi@gmail.com>
2022-04-21 21:02:16 +0430

    new way to generate map

--- src/main/java/Controller/game/GameController.java
+++ src/main/java/Controller/game/GameController.java
@@ -3,6 +3,8 @@ package Controller.game;
 import Model.*;
 
 import Enum.*;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -10,7 +12,7 @@ import java.util.random.*;
 
 public class GameController {
     private static final int mapWidth = 10 ;
-    private static final int mapHeight = 6 ;
+    private static final int mapHeight = 12 ;
     private Terrain[][] map;
     private ArrayList<Civilization> civilizations;
     private int time;
@@ -39,29 +41,64 @@ public class GameController {
             return map[y][x-1];
     }
 
-    private TypeOfTerrain generateTypeOfTerrain (Location location){
+    private ArrayList<TypeOfTerrain> getAreaTypeOfTerrains (Location location){
+        int x = location.getX();
+        int y = location.getY();
+        ArrayList<TypeOfTerrain> out = new ArrayList<TypeOfTerrain>() ;
+        int upperRow = Math.max(0 , y-1);
+        int leftCol = Math.max(0 , x-1);
+        for (int row = upperRow ; row<=y ; row++){
+            for (int col = leftCol ; col<=x ; col++ ){
+                if (row!=y || col!=x)
+                    out.add(map[row][col].getTypeOfTerrain());
+            }
+        }
+        return out;
+    }
+
+
+    private TypeOfTerrain generateTypeOfTerrain (final Location location){
+        int x = location.getX();
+        int y = location.getY();
+
         Random rand = new Random();
         int randNum = rand.nextInt(typeOfTerrains.length);
-
         TypeOfTerrain out = typeOfTerrains[randNum];
-        Terrain previousTerrain = getPreviousTerrain(location);
 
-        if (previousTerrain==null || previousTerrain.getTypeOfTerrain()==out)
+        ArrayList<TypeOfTerrain> areaTypeOfTerrains = getAreaTypeOfTerrains(location);
+        System.out.println(areaTypeOfTerrains.stream().toList());
+        if (areaTypeOfTerrains.isEmpty() || areaTypeOfTerrains.contains(out))
             return out;
 
-        while (true){
-            TypeOfTerrain prev = previousTerrain.getTypeOfTerrain();
-            if ((prev==TypeOfTerrain.DESERT && out==TypeOfTerrain.OCEAN) ||
-                (prev==TypeOfTerrain.OCEAN && out==TypeOfTerrain.DESERT)  )
-                out = typeOfTerrains[rand.nextInt(typeOfTerrains.length)];
-            else if ((prev==TypeOfTerrain.DESERT && (out==TypeOfTerrain.SNOW||out==TypeOfTerrain.TUNDRA)) ||
-                     (out==TypeOfTerrain.DESERT && (prev==TypeOfTerrain.SNOW||prev==TypeOfTerrain.TUNDRA)) )
-                out = typeOfTerrains[rand.nextInt(typeOfTerrains.length)];
+        boolean shouldChangeOut = true;
+        while (shouldChangeOut){
+            out = typeOfTerrains[rand.nextInt(typeOfTerrains.length)];
+            System.out.println("has choose " + out);
+
+            if (out==TypeOfTerrain.OCEAN && (x>3 && x<mapWidth-3) && (y>3 && y<mapHeight-3))
+                shouldChangeOut = true;
+            else if (out==TypeOfTerrain.OCEAN && areaTypeOfTerrains.contains(TypeOfTerrain.DESERT))
+                shouldChangeOut = true;
+
+            else if (out==TypeOfTerrain.DESERT && areaTypeOfTerrains.contains(TypeOfTerrain.OCEAN) )
+                shouldChangeOut = true;
+            else if (out==TypeOfTerrain.DESERT && areaTypeOfTerrains.contains(TypeOfTerrain.SNOW))
+                shouldChangeOut = true;
+            else if (out==TypeOfTerrain.DESERT && areaTypeOfTerrains.contains(TypeOfTerrain.TUNDRA))
+                shouldChangeOut = true;
+            else if (out==TypeOfTerrain.DESERT && areaTypeOfTerrains.contains(TypeOfTerrain.GRASSLAND))
+                shouldChangeOut = true;
+
+            else if (out==TypeOfTerrain.TUNDRA && areaTypeOfTerrains.contains(TypeOfTerrain.DESERT))
+                shouldChangeOut = true;
+
+            else if (out==TypeOfTerrain.SNOW && areaTypeOfTerrains.contains(TypeOfTerrain.DESERT))
+                shouldChangeOut = true;
 
             else
-                return out;
+                shouldChangeOut = false;
         }
-
+        return out;
     }
 
     private void initializeMap (){
@@ -75,7 +112,7 @@ public class GameController {
         for (int y=0 ; y<mapHeight ; y++){
             for (int x=0 ; x<mapWidth ; x++){
                 typeOfTerrainUsed = generateTypeOfTerrain(new Location(x,y)) ;
-
+                System.out.println("added " + typeOfTerrainUsed + " to location ("+x+","+y+")");
                 map[y][x] = new Terrain(typeOfTerrainUsed , null
                         , true ,
                         null , new Location(x,y) , null) ;
@@ -103,8 +140,10 @@ public class GameController {
 
     public void printMap(){
         for (int y=0 ; y<mapHeight ; y++){
+            if (y%2==1)
+                System.out.print("\t");
             for (int x=0 ; x<mapWidth ; x++)
-                System.out.printf("|(%d,%d) : %10s+%12s|\t" ,map[y][x].getLocation().getX() , map[y][x].getLocation().getY(), map[y][x].getTypeOfTerrain() , map[y][x].getTerrainFeatures());
+                System.out.printf("%10s" , map[y][x].getTypeOfTerrain());
             System.out.println();
         }
     }
