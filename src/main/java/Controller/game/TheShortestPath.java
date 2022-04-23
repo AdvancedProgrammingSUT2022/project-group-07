213beacfe103cb06cf0d325b85d29c9f2d15092b
Mana <mana@example.com>
2022-04-23 18:23:56 +0430

    TheShortestPath class added

--- src/main/java/Controller/game/TheShortestPath.java
+++ src/main/java/Controller/game/TheShortestPath.java
@@ -1,2 +1,31 @@
-package Controller.game;public class TheShortestPath {
+package Controller.game;
+
+import Model.Terrain;
+
+public class TheShortestPath {
+    // TODO type fields
+    static int height = GameController.getMapHeight();
+    static int width = GameController.getMapWidth();
+    private static int[][] mpMap = new int[height * width][height * width];
+    private static int[][] distance = new int[height * width][height * width];
+    private static int[][] nextTerrain = new int[height * width][height * width];
+
+    public static void run() {
+        initialiseMpMap();
+        initialiseDistances();
+        findTheShortestPath();
+    }
+
+    private static void initialiseMpMap() {
+
+    }
+
+
+    private static void initialiseDistances() {
+
+    }
+
+    private static void findTheShortestPath() {
+
+    }
 }
