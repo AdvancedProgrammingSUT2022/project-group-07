3a862e90a766094bca35adba4d8390387b010d85
Danial Gharib <danial.mail.gh@gmail.com>
2022-05-03 04:06:57 +0430

    مامان نیوتن باعث شد نیوتن بدنیا بیاد

--- src/main/java/Controller/game/update/UpdateCivilizationElements.java
+++ src/main/java/Controller/game/update/UpdateCivilizationElements.java
@@ -1,4 +1,62 @@
 package Controller.game.update;
 
+import Model.City;
+import Model.Civilization;
+import Model.Technology;
+
+import java.util.ArrayList;
+
 public class UpdateCivilizationElements {
+
+    /**
+     * a function to update whole food of a civilization
+     */
+    public static void updateFood(Civilization civilization){
+        ArrayList<City> cities = civilization.getCities();
+        int sum = 0 ;
+        for (City city : cities)
+            sum += city.getFood();
+        civilization.setFood(civilization.getFood()+sum);
+    }
+
+    /**
+     *  a function to update whole gold of a civilization
+     */
+    public static void updateGold(Civilization civilization){
+        ArrayList<City> cities = civilization.getCities();
+        int sum = 0 ;
+        for (City city : cities)
+            sum += city.getGold();
+        civilization.setGold(civilization.getGold()+sum);
+    }
+
+    /**
+     * a function to update status of a current research
+     */
+    public static void updateResearch(Civilization civilization){
+        Technology currentResearch = civilization.getCurrentResearch();
+        if (currentResearch==null)
+            return;
+        if (currentResearch.getRemainingTurns()==0) {
+            civilization.addTechonolgy(currentResearch);
+            civilization.setCurrentResearch(null);
+        }
+        else
+            currentResearch.setRemainingTurns(currentResearch.getRemainingTurns()-1);
+    }
+
+    public static void updateScience(Civilization civilization){
+        int numberOfCitizens = 0;
+        for (City city : civilization.getCities()) {
+            numberOfCitizens += city.getCitizens().size();
+        }
+        civilization.setScience(civilization.getScience() + numberOfCitizens + 3);
+    }
+
+    public static void update(Civilization civilization) {
+        updateResearch(civilization);
+        updateScience(civilization);
+        updateFood(civilization);
+        updateGold(civilization);
+    }
 }
