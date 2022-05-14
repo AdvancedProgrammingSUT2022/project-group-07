747645871a0a9a64caad401eec38e903de83a3e7
Danial Gharib <danial.mail.gh@gmail.com>
2022-05-14 14:07:14 +0430

    some chert changes

--- src/main/java/Controller/game/update/UpdateCityElements.java
+++ src/main/java/Controller/game/update/UpdateCityElements.java
@@ -111,7 +111,7 @@ public class UpdateCityElements {
             int food = 0;
             int production = 0;
             for (Citizen citizen : city.getCitizens()) {
-                if (citizen.getTerrain() == null) food += 1;
+                if (citizen.getTerrain() == null) production += 1;
                 TerrainOutput terrainOutput = TerrainController.getTerrainsOutput(civilization , citizen.getTerrain());
                 gold += terrainOutput.getGold();
                 food += terrainOutput.getFood();
