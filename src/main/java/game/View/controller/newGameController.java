30fd84813e9be8f0934be36af9c90513ec54580e
Danial Gharib <90343552+Danial-Gharib@users.noreply.github.com>
2022-07-15 16:53:13 +0430

    Baz ham aqa danial (#80)
    مرغ مگس خوار

--- src/main/java/game/View/controller/newGameController.java
+++ src/main/java/game/View/controller/newGameController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
