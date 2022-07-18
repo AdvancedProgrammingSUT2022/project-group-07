4f1b968768206057300bdb4d9d4e561e230e5a55
Mahan-Bayhaghi <mahan.bayhaghi@gmail.com>
2022-07-18 10:15:27 +0430

    info panel is a little bit better now

--- src/main/java/game/View/controller/gamePageController.java
+++ src/main/java/game/View/controller/gamePageController.java
@@ -2,22 +2,16 @@ package game.View.controller;
 
 import game.Controller.game.GameController;
 import game.Controller.game.SelectController;
-import game.Controller.game.UnitController;
 import game.Enum.TypeOfTechnology;
-import game.Enum.TypeOfUnit;
-import game.Enum.UnitStatus;
 import game.Main;
-import game.Model.Location;
 import game.Model.Technology;
 import game.Model.Terrain;
 import game.Model.Unit;
-import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.geometry.Orientation;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -123,26 +117,22 @@ public class gamePageController implements Initializable {
         currentResearchImageView.setFitWidth(100);
         currentResearchImageView.setPreserveRatio(true);
         currentResearchImageView.setSmooth(true);
-        try {
-            currentResearchImageView.setImage(
-                    new Image(
-                            Main.class.getResource("/game/images/technologies/" + GameController.getInstance().getCurrentCivilization().getCurrentResearch().getTypeOfTechnology().getName() + ".png").toExternalForm()
-                    )
-            );
-        } catch (Exception e){
-            currentResearchImageView.setImage(
-                    new Image(
-                            Main.class.getResource("/game/images/technologies/Agriculture.png").toExternalForm()
-                    )
-            );
-        }
 
         // TODO : next line should be deleted in real game !
         GameController.getInstance().getCurrentCivilization().setCurrentResearch(new Technology(TypeOfTechnology.AGRICULTURE));
 
-        int predictedTurns = GameController.getInstance().getCurrentCivilization().getCurrentResearch().getTypeOfTechnology().getScienceNeeded() / GameController.getInstance().getCurrentCivilization().getScience() ;
-        double progress = ((double) GameController.getInstance().getCurrentCivilization().getCurrentResearch().getRemainingTurns()) / ((double) predictedTurns );
-        progressBar.setProgress(progress);
+        try {
+            currentResearchImageView.setImage(new Image(
+                    Main.class.getResource("/game/images/technologies/" + GameController.getInstance().getCurrentCivilization().getCurrentResearch().getTypeOfTechnology().getName() + ".png").toExternalForm())) ;
+            int predictedTurns = GameController.getInstance().getCurrentCivilization().getCurrentResearch().getTypeOfTechnology().getScienceNeeded() / GameController.getInstance().getCurrentCivilization().getScience() ;
+            double progress = ((double) GameController.getInstance().getCurrentCivilization().getCurrentResearch().getRemainingTurns()) / ((double) predictedTurns );
+            progressBar.setProgress(progress);
+            progressBar.setDisable(false);
+        } catch (NullPointerException e){
+            currentResearchImageView.setImage(new Image(
+                    Main.class.getResource("/game/images/icons/Research.png").toExternalForm()));
+            progressBar.setDisable(true);
+        }
         progressBar.setStyle("-fx-background-color: Red ; -fx-background-radius: 20 ;");
         progressBar.setTooltip(new Tooltip(GameController.getInstance().getCurrentCivilization().getCurrentResearch().getTypeOfTechnology().getName()));
     }
@@ -159,24 +149,19 @@ public class gamePageController implements Initializable {
         selectedUnitImageView.setPreserveRatio(true);
         selectedUnitImageView.setSmooth(true);
         try {
-            selectedUnitImageView.setImage(
-                    new Image(
-                            Main.class.getResource("/game/assets/civAsset/units/Units/" + GameController.getInstance().getCurrentCivilization().getUnits().get(0) + ".png").toExternalForm()
-                    )
-            );
-        } catch (Exception e){
-            selectedUnitImageView.setImage(
-                    new Image(
-                            Main.class.getResource("/game/assets/civAsset/units/Units/Settler.png").toExternalForm()
-                    )
-            );
+            selectedUnitImageView.setImage(new Image(
+                    Main.class.getResource("/game/assets/civAsset/units/Units/" + GameController.getInstance().getCurrentCivilization().getUnits().get(0) + ".png").toExternalForm()));
+            Unit selectedUnit = SelectController.selectedUnit ;
+            String unitData = String.format("HP : %d\nMP : %d\nStatus : %s" , selectedUnit.getHp() , selectedUnit.getMp() , selectedUnit.getUnitStatus());
+            selectedUnitDataLabel.setText(unitData);
+            selectedUnitPanel.setDisable(false);
+        } catch (NullPointerException e){
+            selectedUnitImageView.setImage(new Image(
+                    Main.class.getResource("/game/images/icons/Unit.png").toExternalForm()));
+            selectedUnitPanel.setDisable(true);
         }
-
         // TODO : next line should be deleted !
-        Unit selectedUnit = new Unit(TypeOfUnit.SETTLER , UnitStatus.ACTIVE , new Location(10,10) , 20 , GameController.getInstance().getCurrentCivilization() , 0) ;
-//        Unit selectedUnit = SelectController.selectedUnit ;
-        String unitData = String.format("HP : %d\nMP : %d\nStatus : %s" , selectedUnit.getHp() , selectedUnit.getMp() , selectedUnit.getUnitStatus());
-        selectedUnitDataLabel.setText(unitData);
+//        Unit selectedUnit = new Unit(TypeOfUnit.SETTLER , UnitStatus.ACTIVE , new Location(10,10) , 20 , GameController.getInstance().getCurrentCivilization() , 0) ;
     }
 
 
