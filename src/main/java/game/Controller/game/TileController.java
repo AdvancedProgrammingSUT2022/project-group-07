cb5d13bf8111bf7dc601ba1d341c97722429b6e8
Mana <mana@example.com>
2022-07-20 10:59:26 +0430

    show tile info

--- src/main/java/game/Controller/game/TileController.java
+++ src/main/java/game/Controller/game/TileController.java
@@ -1,29 +1,28 @@
 package game.Controller.game;
 
+import game.Enum.Resources;
 import game.Enum.TerrainFeatures;
 import game.Enum.TypeOfTerrain;
 import game.View.components.Tile;
-import javafx.scene.shape.Polygon;
 
 public class TileController {
-    public static Tile createTile(TypeOfTerrain typeOfTerrainUsed, TerrainFeatures typeOfTerrainFeatureUsed, int x, int y) {
+    public static Tile createTile(TypeOfTerrain typeOfTerrainUsed, TerrainFeatures typeOfTerrainFeatureUsed, int x, int y, Resources resources) {
 
         double yCoord = x * Tile.getTileWidth() + (y % 2) * Tile.getN() + 10;
         double xCoord = y * Tile.getTileHeight() * 0.75 + 10;
 
         Tile tile = new Tile(xCoord, yCoord);
-        findBackGround(tile , typeOfTerrainUsed , typeOfTerrainFeatureUsed);
+        findBackGround(tile , typeOfTerrainUsed , typeOfTerrainFeatureUsed, resources);
         return tile;
     }
 
-    private static void findBackGround(Tile tile, TypeOfTerrain typeOfTerrainUsed , TerrainFeatures typeOfTerrainFeatureUsed) {
+    private static void findBackGround(Tile tile, TypeOfTerrain typeOfTerrainUsed , TerrainFeatures typeOfTerrainFeatureUsed, Resources resources) {
         String address1, address2 = null;
         address1 = typeOfTerrainUsed.getName() + ".png";
 
         if (typeOfTerrainFeatureUsed != null) {
-//            System.out.println("Type of feature = " + typeOfTerrainFeatureUsed.getName() + "    Terrain = " + typeOfTerrainUsed.getName());
             address2 = typeOfTerrainUsed.getName() + "+" + typeOfTerrainFeatureUsed + ".png";
         }
-        tile.setBackground(address1, address2);
+        tile.setBackground(address1, address2, resources, typeOfTerrainUsed, typeOfTerrainFeatureUsed);
     }
 }
